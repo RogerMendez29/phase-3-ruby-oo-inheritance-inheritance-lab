30257527e6e4864c20e55f985265197dc7f5231f
Roger Mendez <roger61087@gmail.com>
2021-12-28 16:24:14 -0500

    commit tree 5b41e357b7e0e018878fe373aaaf94ca4645d707

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./user/"
+
+class Student < User
+    def initialize
+        @knowledge =[]
+        
+    end
+    def learn (topic)
+        @knowledge << topic
+    end
+    def knowledge
+        @knowledge
+        
+    end
+    
+    
+    
 
 end
